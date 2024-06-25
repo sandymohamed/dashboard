b0322f2c0b34a9ac859df310da00edf393adb8dc
OmarElmoez <omarelmoez5@gmail.com>
2024-06-25 16:18:04 +0200

    install svgr and tsConfigPaths and solve the issue with the .eslintrc.cjs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// svgr allows you to import SVG files as React components
+import svgr from 'vite-plugin-svgr'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr(), tsconfigPaths()],
 })
